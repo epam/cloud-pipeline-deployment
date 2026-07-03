658f282ada46b6be3c404eb227ab4ac301eec12d
Oleksii Martyniuk <143590000+oleksii-mart@users.noreply.github.com>
2026-07-03 16:30:22 +0300

    added hostpath to saml-idp-profiles.json (#9)

--- helm/cloud-pipeline/cp-idp/files/hooks/register-app-in-idp.sh
+++ helm/cloud-pipeline/cp-idp/files/hooks/register-app-in-idp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 kubectl -n "$NAMESPACE" exec deployment/cp-idp -- bash -c \
-  "saml-idp add-connection https://${API_SRV_HOST}:${API_SRV_PORT}/pipeline/ -c /opt/idp/pki/sso-public-cert.pem --profileDatabase /opt/idp/saml-idp-profiles.json"
+  "saml-idp add-connection https://${API_SRV_HOST}:${API_SRV_PORT}/pipeline/ -c /opt/idp/pki/sso-public-cert.pem --profileDatabase \${CP_IDP_PROFILE_DB}"
 echo "IdP connection for API server https://${API_SRV_HOST}:${API_SRV_PORT} added"
--- helm/cloud-pipeline/cp-idp/files/hooks/register-gitlab-in-idp.sh
+++ helm/cloud-pipeline/cp-idp/files/hooks/register-gitlab-in-idp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 kubectl -n "$NAMESPACE" exec deployment/cp-idp -- sh -c \
-  'saml-idp add-connection "$1" -c /opt/idp/pki/sso-public-cert.pem --profileDatabase /opt/idp/saml-idp-profiles.json' \
+  'saml-idp add-connection "$1" -c /opt/idp/pki/sso-public-cert.pem --profileDatabase $CP_IDP_PROFILE_DB' \
   _ "$ISSUER"
 echo "IdP connection for GitLab issuer $ISSUER added"
